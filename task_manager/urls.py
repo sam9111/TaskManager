da9d39219cb79a2a102233d94748c5f9f69e6c51
sam9111 <samyuktha19095@cse.ssn.edu.in>
2022-01-29 16:02:26 +0530

    added all views and paths definitions

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 from django.urls import path
 
 
+from tasks.views import *
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # Add all your views here
+    path("tasks/", tasks_view),
+    path("add-task/", add_task_view),
+    path("delete-task/<int: index>/", delete_task_view),
+    path("complete_task/<int: index>/", complete_task_view),
+    path("completed_tasks/", completed_tasks_view),
+    path("all_tasks/", all_tasks_view),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,1 +1,22 @@
-# Add all your views here
+def tasks_view(request):
+    pass
+
+
+def add_task_view(request):
+    pass
+
+
+def delete_task_view(request):
+    pass
+
+
+def complete_task_view(request):
+    pass
+
+
+def completed_tasks_view(request):
+    pass
+
+
+def all_tasks_view(request):
+    pass
