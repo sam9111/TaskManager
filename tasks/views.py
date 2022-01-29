7855cbdad23c8afe06559fd3c2c3f2e888293ff3
sam9111 <samyuktha19095@cse.ssn.edu.in>
2022-01-29 16:34:53 +0530

    working add and delete views

--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+
+tasks = []
+
+
 def tasks_view(request):
-    pass
+    return render(request, "tasks.html", {"tasks": tasks})
 
 
-def add_task_view(request):
-    pass
+def add_task_view(
+    request,
+):
+    task_value = request.GET.get("task")
+    tasks.append(task_value)
+    return HttpResponseRedirect("/tasks")
 
 
-def delete_task_view(request):
-    pass
+def delete_task_view(request, index):
+    del tasks[index - 1]
+    return HttpResponseRedirect("/tasks")
 
 
 def complete_task_view(request):
